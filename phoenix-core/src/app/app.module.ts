3c2bfb95b000cfb87341cd5f1c3a12baa0cd9062
Lucas Ma <mayh11@lenovo.com>
2019-07-23 11:28:02 +0800

    create js

--- phoenix-core/src/app/app.module.ts
+++ phoenix-core/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SecurityAdvisorComponent } from './security-advisor/security-advisor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecurityAdvisorComponent
   ],
   imports: [
     BrowserModule,
