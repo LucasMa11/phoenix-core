4a02b96a1294b8dcb449313b70118854aa880829
Lucas Ma <mayh11@lenovo.com>
2019-07-23 18:59:01 +0800

    module

--- combination.js
+++ combination.js
@@ -4,18 +4,13 @@ var path = require('path');
 const { exec } = require('child_process');
 var destPath = path.join(__dirname, "phoenix-core/src/app/SA-feature");
 if(fs.existsSync(destPath) == false){
-    exec("git clone https://github.com/LucasMa11/SA-feature.git", {cwd: './phoenix-core/src/app/'} , function(err,stderr){
-	if(err) {
-		console.log('git clone error:' + stderr);
-	} 
-    });
+	exec("git clone https://github.com/LucasMa11/SA-feature.git", {cwd: './phoenix-core/src/app/beta/'});
+	exec("git clone https://github.com/LucasMa11/SA-feature.git", {cwd: './phoenix-core/src/app/prod/'});
 } else {
-    exec("git pull", {cwd: './phoenix-core/src/app/SA-feature/'} , function(err,stderr){
-	if(err) {
-		console.log('git pull error:' + stderr);
-	} 
-    });
+	exec("git pull", {cwd: './phoenix-core/src/app/beta/SA-feature/'});
+	exec("git pull", {cwd: './phoenix-core/src/app/prod/SA-feature/'});
 }
+exec("git checkout develop", {cwd:'./phoenix-core/src/app/beta/SA-feature/'});
 exec("ng s --o",{cwd: '../phoenix-core/phoenix-core/'} , function(err,stdout,stderr){
 	if(err) {
 		console.log('error:'+stderr);
