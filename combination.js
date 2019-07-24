6b939a212e612057be93acac3a8e38e018527f7d
Lucas Ma <mayh11@lenovo.com>
2019-07-24 17:28:34 +0800

    done

--- combination.js
+++ combination.js
@@ -2,21 +2,27 @@ var fs = require('fs');
 var path = require('path');
 
 const { exec } = require('child_process');
-var destPath = path.join(__dirname, "phoenix-core/src/app/SA-feature");
-if(fs.existsSync(destPath) == false){
+var betaPath = path.join(__dirname, "phoenix-core/src/app/beta/SA-feature");
+var prodPath = path.join(__dirname, "phoenix-core/src/app/prod/SA-feature");
+if(fs.existsSync(betaPath) == false || fs.existsSync(prodPath) == false){
 	exec("git clone https://github.com/LucasMa11/SA-feature.git", {cwd: './phoenix-core/src/app/beta/'});
 	exec("git clone https://github.com/LucasMa11/SA-feature.git", {cwd: './phoenix-core/src/app/prod/'});
+	console.log("git clone finished")
 } else {
 	exec("git pull", {cwd: './phoenix-core/src/app/beta/SA-feature/'});
 	exec("git pull", {cwd: './phoenix-core/src/app/prod/SA-feature/'});
+	console.log("git pull finished")
 }
 exec("git checkout develop", {cwd:'./phoenix-core/src/app/beta/SA-feature/'});
-exec("git pull", {cwd: './phoenix-core/src/app/beta/SA-feature/'});
+console.log("checkout to beta version branch");
+// console.log("wait for npm");
+// exec("npm i", {cwd:'./phoenix-core/'});
+// console.log("npm i done")
 exec("ng s --o",{cwd: '../phoenix-core/phoenix-core/'} , function(err,stdout,stderr){
 	if(err) {
-		console.log('error:'+stderr);
+		console.log('ng serve error:'+stderr);
 	} else {
-		console.log('success'+stdout);
+		console.log('ng serve success'+stdout);
 	}
 });
 
